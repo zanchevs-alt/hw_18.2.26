61e4dd53f8cbf447a26edaa4240a64d3c1db7756
marga <zanchevs@post.bgu.ac.il>
2026-03-29 02:07:51 +0300

    25

---
+++ hw1.py
@@ -0,0 +1,33 @@
+"""
+Python Sets – Homework Set Group Operations
+Solve at least 1 question
+
+Part A
+For each question: What is the missing operation (?)
+
+Example:
+
+{1, 2} ? {3, 4} = {1, 2, 3, 4}
+
+Answer: | , because-
+
+{1, 2} | {3, 4} = {1, 2, 3, 4}
+
+Solve:
+
+{1, 2} | {3, 4} = {1, 2, 3, 4}
+
+{1, 2, 3} & {3, 4} = {3}
+
+{1, 2, 3, 4} ^ {3, 4} = {1, 2}
+
+{1, 2, 3} ^ {3, 4} = {1, 2, 4}
+
+{1, 2, 3, 4} >= {1, 2} = True
+
+{1, 2} <= {1, 2, 3, 4} = True
+
+{5, 6} | {6, 7} = {5, 6, 7}
+
+{10, 11, 12} - {12, 13} = {10, 11}
+"""
--- main.py
+++
@@ -1,5 +0,0 @@
-number: int = int(input("number?"))
-if number % 2 == 0:
-    print("even")
-else:
-    print("odd")
